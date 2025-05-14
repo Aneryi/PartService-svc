f7eef42f6b35709336ff5d4c5b092a44ad75923d
Aneryi <58134509+Aneryi@users.noreply.github.com>
2025-05-14 14:18:14 +0800

    Init

---
+++ src/test/java/com/partservice/partservice/PartServiceSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partservice.partservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartServiceSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
